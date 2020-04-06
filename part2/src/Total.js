36e76e35b3058f30853715f0e8777c0907be3233
Swetha Ganapathy subramanian <swethaganapathy2@gmail.com>
2020-04-06 15:34:34 -0700

    exercise 2.3

--- part2/src/Total.js
+++ part2/src/Total.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 
 const Total = ({ parts }) => {
+    const total = parts.reduce((total, part) => total + part.exercises, 0)
     return (
         <p><strong>
-            total of {parts.reduce((total, part) => total + part.exercises, 0)} exercises </strong></p>
+            total of {total} exercises </strong></p>
     )
 
 }
